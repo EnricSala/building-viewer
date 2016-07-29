cdbe245bf24b777d66f2e6996f43af385e53cee6
Enric Sala <enric.sala@mcia.upc.edu>
2016-07-29 17:50:32 +0200

    Update metrics controller and stop masking errors from the repository

--- backend/src/main/java/mcia/building/viewer/controller/MetricsController.java
+++ backend/src/main/java/mcia/building/viewer/controller/MetricsController.java
@@ -1,36 +1,31 @@
 package mcia.building.viewer.controller;
 
-import java.util.Collections;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import mcia.building.viewer.controller.dto.CurrentMetricsRequest;
 import mcia.building.viewer.controller.dto.CurrentMetricsResponse;
 import mcia.building.viewer.metrics.MetricsRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import rx.Observable;
 
 @RestController
 @RequestMapping("/api/metrics")
+@RequiredArgsConstructor
 @Slf4j
 public class MetricsController {
 
-	@Autowired
-	private MetricsRepository metricsRepository;
+	private final MetricsRepository metricsRepository;
 
-	@RequestMapping(value = "/current", method = RequestMethod.POST)
-	public Observable<CurrentMetricsResponse> getCurrent(@RequestBody CurrentMetricsRequest request) {
-		log.info("GET /metrics/current with {}", request);
+	@PostMapping("/current")
+	public Observable<CurrentMetricsResponse> currentMetrics(
+			@RequestBody CurrentMetricsRequest request) {
+
+		log.info("POST /metrics/current to read {}", request);
 		return metricsRepository
 				.queryLastPoint(request.getMetricIds())
-				.onErrorReturn(err -> {
-					log.error("Error reading current metrics", err);
-					return Collections.emptyMap();
-				})
 				.map(CurrentMetricsResponse::new);
 	}
 
