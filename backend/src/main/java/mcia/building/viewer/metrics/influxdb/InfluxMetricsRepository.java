5182f80faee7b2b0a6e354d8d5a59f0c7fa14b0e
Enric Sala <enric.sala@mcia.upc.edu>
2016-08-04 12:14:32 +0200

    Improve influx metrics repository test and robustness

--- backend/src/main/java/mcia/building/viewer/metrics/influxdb/InfluxMetricsRepository.java
+++ backend/src/main/java/mcia/building/viewer/metrics/influxdb/InfluxMetricsRepository.java
@@ -1,33 +1,32 @@
 package mcia.building.viewer.metrics.influxdb;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import mcia.building.viewer.domain.NamedPoint;
+import mcia.building.viewer.domain.Point;
+import mcia.building.viewer.metrics.MetricsRepository;
+import org.influxdb.InfluxDB;
+import org.influxdb.dto.Serie;
+import rx.Observable;
+
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.StringJoiner;
 import java.util.concurrent.TimeUnit;
 
-import org.influxdb.InfluxDB;
-import org.influxdb.dto.Serie;
-
-import lombok.extern.slf4j.Slf4j;
-import mcia.building.viewer.domain.NamedPoint;
-import mcia.building.viewer.domain.Point;
-import mcia.building.viewer.metrics.MetricsRepository;
-import rx.Observable;
-
+@RequiredArgsConstructor
 @Slf4j
 public class InfluxMetricsRepository implements MetricsRepository {
 
 	private final InfluxDB influx;
 	private final String database;
 
-	public InfluxMetricsRepository(InfluxDB influx, String database) {
-		this.influx = influx;
-		this.database = database;
-	}
-
 	@Override
 	public Observable<Map<String, Point>> queryLastPoint(List<String> ids) {
-		log.debug("Query last point from {} series", ids.size());
+		log.debug("Query last point from {}", ids);
+		if (ids.isEmpty())
+			return Observable.just(Collections.emptyMap());
 		return buildQuery(ids)
 				.doOnNext(query -> log.debug("Influx query: {}", query))
 				.map(query -> influx.query(database, query, TimeUnit.MILLISECONDS))
@@ -38,12 +37,12 @@ public class InfluxMetricsRepository implements MetricsRepository {
 	}
 
 	private Observable<String> buildQuery(List<String> ids) {
-		final String prefix = "select time, value from ";
-		final String sufix = " limit 1";
+		final String prefix = "select time, value from \"";
+		final String sufix = "\" limit 1";
 		return Observable
 				.from(ids)
 				.map(this::cleanMetricId)
-				.reduce(new StringJoiner(", ", prefix, sufix), StringJoiner::add)
+				.reduce(new StringJoiner("\", \"", prefix, sufix), StringJoiner::add)
 				.map(StringJoiner::toString);
 	}
 
--- backend/src/test/java/mcia/building/viewer/metrics/influxdb/InfluxMetricsRepositoryTest.java
+++ backend/src/test/java/mcia/building/viewer/metrics/influxdb/InfluxMetricsRepositoryTest.java
@@ -3,15 +3,19 @@ package mcia.building.viewer.metrics.influxdb;
 import mcia.building.viewer.domain.Point;
 import mcia.building.viewer.metrics.MetricsRepository;
 import org.influxdb.InfluxDB;
+import org.influxdb.dto.Serie;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import rx.observers.TestSubscriber;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class InfluxMetricsRepositoryTest {
 
@@ -21,12 +25,12 @@ public class InfluxMetricsRepositoryTest {
 	private static final String TEST_USER = "root";
 	private static final String TEST_PASSWORD = "root";
 
-	private static final List<String> series = Arrays.asList("test1", "test2");
-
+	private static InfluxDB influxTemplate;
 	private static MetricsRepository metrics;
 
 	@BeforeClass
 	public static void setup() throws Exception {
+		// Configure InfluxDB client and UUT
 		InfluxDbConfiguration config = new InfluxDbConfiguration();
 		config.setHost(TEST_HOST);
 		config.setPort(TEST_PORT);
@@ -34,15 +38,32 @@ public class InfluxMetricsRepositoryTest {
 		config.setPassword(TEST_PASSWORD);
 		config.setDatabase(TEST_DB);
 		config.afterPropertiesSet();
-
-		InfluxDB influxTemplate = config.influxTemplate();
+		influxTemplate = config.influxTemplate();
 		metrics = config.influxMetricsRepository(influxTemplate);
+
+		// Init database with some points
+		List<Serie> list = new ArrayList<>();
+		list.add(new Serie.Builder("test1").columns("time", "value")
+				.values(System.currentTimeMillis(), 23.7).build());
+		list.add(new Serie.Builder("test2").columns("time", "value")
+				.values(System.currentTimeMillis(), 25.2).build());
+		list.add(new Serie.Builder("white-list_ed charac.ters").columns("time", "value")
+				.values(System.currentTimeMillis(), -5.4).build());
+		Serie[] series = new Serie[list.size()];
+		list.toArray(series);
+		influxTemplate.write(TEST_DB, TimeUnit.MILLISECONDS, series);
+	}
+
+	@AfterClass
+	public static void teardown() {
+		influxTemplate.deleteDatabase("test-database");
 	}
 
 	@Test
 	public void shouldQueryMultipleSeries() {
-		Map<String, Point> result = metrics.queryLastPoint(series).toBlocking().single();
-		assertEquals(series.size(), result.size());
+		Map<String, Point> result = metrics.queryLastPoint(
+				Arrays.asList("test1", "test2")).toBlocking().single();
+		assertEquals(2, result.size());
 	}
 
 	@Test
@@ -54,9 +75,23 @@ public class InfluxMetricsRepositoryTest {
 
 	@Test
 	public void shouldRemoveInvalidCharacters() {
-		Map<String, Point> result =
-				metrics.queryLastPoint(Arrays.asList("t@\"est#1")).toBlocking().single();
+		Map<String, Point> result = metrics.queryLastPoint(
+				Arrays.asList("t@e\":$st#1")).toBlocking().single();
 		assertEquals(1, result.size());
+	}
+
+	@Test
+	public void shouldAllowWhitelistedCharacters() {
+		Map<String, Point> result = metrics.queryLastPoint(
+				Arrays.asList("white-list_ed charac.ters")).toBlocking().single();
+		assertEquals(1, result.size());
+	}
+
+	@Test
+	public void emptyQueryShouldWork() {
+		Map<String, Point> result = metrics.queryLastPoint(
+				Arrays.asList()).toBlocking().single();
+		assertEquals(0, result.size());
 	}
 
 }
